b1c3bc159c1432bb2e609f14b4d5208ef5923ada
orutanakaito <voltgt777@gmail.com>
2024-06-16 17:27:07 +0900

    東を選択するとプレイヤーは森に入り武器のロングソードを入手できるようにしました

--- src/main/java/handler/ChoiceHandler.java
+++ src/main/java/handler/ChoiceHandler.java
@@ -19,8 +19,9 @@ public class ChoiceHandler implements ActionListener {
         // 選択されたボタンのコマンドの文字列がこの変数に入る
         String yourChoice = e.getActionCommand();
 
+        // プレイヤーの現在地
         switch (gameWindow.getPosition()) {
-            case "町の門":
+            case "町の門" -> {
                 switch (yourChoice) {
                     case "c1" -> gameWindow.talkGuard();
                     case "c2" -> gameWindow.attackGuard();
@@ -28,23 +29,26 @@ public class ChoiceHandler implements ActionListener {
                     case "c4" -> {
                     }
                 }
-                break;
-            case "門番に話す", "門番に攻撃":
+            }
+            case "門番に話す", "門番に攻撃" -> {
                 switch (yourChoice) {
                     case "c1" -> gameWindow.townGate();
                 }
-                break;
-            case "分かれ道":
+            }
+            case "分かれ道" -> {
                 switch (yourChoice) {
-                    case "c1":
-                        gameWindow.north();
-                        break;
-                    case "c2", "c4":
-                        break;
-                    case "c3":
-                        gameWindow.townGate();
-                        break;
+                    case "c1" -> gameWindow.north();
+                    case "c2" -> gameWindow.east();
+                    case "c3" -> gameWindow.townGate();
+                    case "c4" -> {
+                    }
                 }
+            }
+            case "北" -> {
+                switch (yourChoice) {
+                    case "c1" -> gameWindow.crossRoad();
+                }
+            }
         }
     }
 }
